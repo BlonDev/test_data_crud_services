de5f74e3d34676a88dd616fa9abeb398323c985f
Guido Barbaglia <guido.barbaglia@geobricks.org>
2016-02-07 19:12:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  client = MongoClient('mongodb://kalimaha:Ce09114238@ds059145.mongolab.com:59145/kalimadata')
+  db = client['kalimadata']
+  collection = db['books']
+  return collection.find()
 
 if __name__ == "__main__":
   app.run() 
