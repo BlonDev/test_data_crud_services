e14a66335b1e874f2a09773a9d083f22c39a1476
Guido Barbaglia <guido.barbaglia@geobricks.org>
2016-02-07 19:18:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+from bson import json_util
 from flask import Flask
 from pymongo import MongoClient
 
@@ -9,7 +11,9 @@ def hello():
   client = MongoClient('mongodb://kalimaha:Ce09114238@ds059145.mongolab.com:59145/kalimadata')
   db = client['kalimadata']
   collection = db['books']
-  return collection.find()
+  books = collection.find()
+  books = json.dumps(books, sort_keys=True, indent=4, default=json_util.default)
+  return Response(books, content_type='application/json; charset=utf-8')
 
 if __name__ == "__main__":
   app.run() 
