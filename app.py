a2e147b3dc89ea00377ea05d14ac99653e3e3524
Kalimaha <guido.barbaglia@gmail.com>
2016-02-09 16:53:46 +0100

    Testing routing.

--- app.py
+++ app.py
@@ -1,35 +1,20 @@
 import json
+from flask import Flask
 from bson import json_util
-from flask import Flask
-from pymongo import MongoClient
-from flask import Flask
 from fake_data_crud_service.rest.books import books
 from fake_data_crud_service.resources.schema import schema
 
 
-class RootREST:
-
-    def __init__(self, host, run_flask):
-        self.host = host
-        self.run_flask = run_flask
-        self.app = Flask(__name__)
-        self.app.register_blueprint(books, url_prefix='/books')
-
-        # # Root service.
-        @self.app.route('/', methods=['GET'])
-        def json_schema_service():
-            return json.dumps(schema, sort_keys=True, indent=4, default=json_util.default),\
-                   200,\
-                   {'Content-Type': 'application/schema+json; charset=utf-8'}
-
-        # Run Flask.
-        if self.run_flask:                              # pragma: no cover
-            # self.app.run(host=self.host, debug=True)    # pragma: no cover
-            self.app.run(debug=True)    # pragma: no cover
+app = Flask(__name__)
+app.register_blueprint(books, url_prefix='/books')
 
 
-def run_engine(host):                                   # pragma: no cover
-    RootREST(host, True)                                # pragma: no cover
+@app.route('/', methods=['GET'])
+def json_schema_service():
+    return json.dumps(schema, sort_keys=True, indent=4, default=json_util.default), \
+           200, \
+           {'Content-Type': 'application/schema+json; charset=utf-8'}
 
-if __name__ == '__main__':                              # pragma: no cover
-    run_engine('0.0.0.0')                               # pragma: no cover
+
+if __name__ == '__main__':
+    app.run()
