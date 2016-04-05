b430e6f6a84b4ff23c209f368037720c60d28652
Kalimaha <guido.barbaglia@gmail.com>
2016-04-05 17:10:17 +1000

    CORS filters

--- fake_data_crud_service/rest/books.py
+++ fake_data_crud_service/rest/books.py
@@ -1,8 +1,9 @@
 import json
-from flask import Response
 from flask import request
-from flask import Blueprint
+from flask import Response
 from bson import json_util
+from flask import Blueprint
+from flask.ext.cors import cross_origin
 from fake_data_crud_service.core.dao import get_dao
 
 
@@ -10,6 +11,7 @@ books = Blueprint('books', __name__)
 
 
 @books.route('/<environment>/', methods=['GET'])
+@cross_origin(origins='*', headers=['Content-Type'])
 def get(environment):
     dao = get_dao(environment)
     out = json.dumps(dao.get('books'), sort_keys=True, indent=4, default=json_util.default)
@@ -17,6 +19,7 @@ def get(environment):
 
 
 @books.route('/<environment>/<item_id>/', methods=['GET'])
+@cross_origin(origins='*', headers=['Content-Type'])
 def get_by_id(environment, item_id):
     dao = get_dao(environment)
     out = json.dumps(dao.get_by_id('books', item_id), sort_keys=True, indent=4, default=json_util.default)
@@ -24,6 +27,7 @@ def get_by_id(environment, item_id):
 
 
 @books.route('/<environment>/<item_id>/', methods=['DELETE'])
+@cross_origin(origins='*', headers=['Content-Type'])
 def delete(environment, item_id):
     dao = get_dao(environment)
     out = json.dumps(dao.delete('books', item_id), sort_keys=True, indent=4, default=json_util.default)
@@ -31,6 +35,7 @@ def delete(environment, item_id):
 
 
 @books.route('/<environment>/<item_id>/', methods=['PUT'])
+@cross_origin(origins='*', headers=['Content-Type'])
 def update(environment, item_id):
     item = json.loads(request.data)
     dao = get_dao(environment)
@@ -40,6 +45,7 @@ def update(environment, item_id):
 
 
 @books.route('/<environment>/', methods=['POST'])
+@cross_origin(origins='*', headers=['Content-Type'])
 def create(environment):
     item = json.loads(request.data)
     dao = get_dao(environment)
